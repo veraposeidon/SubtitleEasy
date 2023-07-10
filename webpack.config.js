30b379fc75ec1ac37476945020dd514e958d7099
shenxiaohai <shenxiaohai@kanyun.com>
2023-07-11 00:43:11 +0800

    feat: update publish steps

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ZipWebpackPlugin = require('zip-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-
-const rimraf = require('rimraf');
 
 module.exports = {
   mode: 'production',
@@ -11,22 +8,10 @@ module.exports = {
     // No entry point needed if you only want to copy files
   },
   plugins: [
-    new CopyWebpackPlugin({
-      // copy subtitle.dual folder to dist except manifest.json
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'subtitle.dual'),
-          to: path.resolve(__dirname, 'dist'),
-          globOptions: {
-            ignore: ['**/manifest.json']
-          }
-        }
-      ]
-    }),
     new WebpackManifestPlugin({
       fileName: 'manifest.json',
       seed: require('./subtitle.dual/manifest.json'),
-      generate: (seed, files) => {
+      generate: (seed) => {
         const manifest = seed || {};
 
         // set version to current git commit tag and rm the prefix 'v'
@@ -42,13 +27,18 @@ module.exports = {
         return manifest;
       }
     }),
-    new ZipWebpackPlugin({
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'subtitle_easy.zip'
+    new CopyWebpackPlugin({
+      // copy subtitle.dual folder to dist except manifest.json
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'subtitle.dual'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+            ignore: ['**/manifest.json']
+          }
+        }
+      ]
     })
   ],
   devtool: 'source-map'
 };
-
-// 清除 dist 文件夹
-rimraf.sync(path.resolve(__dirname, 'dist'));
