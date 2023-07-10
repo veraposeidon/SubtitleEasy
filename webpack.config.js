d434bbd1bdbb3eb1e193f80d7608367cf832fdf5
shenxiaohai <shenxiaohai@kanyun.com>
2023-07-10 23:49:12 +0800

    feat: auto bump version

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ZipWebpackPlugin = require('zip-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
 const rimraf = require('rimraf');
 
 module.exports = {
@@ -10,7 +12,35 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [{ from: 'subtitle.dual', to: './' }]
+      // copy subtitle.dual folder to dist except manifest.json
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'subtitle.dual'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+            ignore: ['**/manifest.json']
+          }
+        }
+      ]
+    }),
+    new WebpackManifestPlugin({
+      fileName: 'manifest.json',
+      seed: require('./subtitle.dual/manifest.json'),
+      generate: (seed, files) => {
+        const manifest = seed || {};
+
+        // set version to current git commit tag and rm the prefix 'v'
+        const version = require('child_process')
+          .execSync('git describe --tags --always')
+          .toString()
+          .trim()
+          .replace(/^v/, '');
+
+        console.log('version', version);
+        manifest.version = version.toString();
+
+        return manifest;
+      }
     }),
     new ZipWebpackPlugin({
       path: path.resolve(__dirname, 'dist'),
